66b3fde85a4147f053259195da584c0e6d014e5c
Pavel Usanli <pavel.usanli@gmail.com>
2026-05-21 12:09:00 +0300

    add Vault LXC (201, 192.168.1.3) with Ansible role

--- terraform/envs/common/main.tf
+++ terraform/envs/common/main.tf
@@ -24,6 +24,24 @@ module "adguard" {
   ssh_public_keys = [var.ssh_public_key]
 }
 
+module "vault" {
+  source = "../../modules/proxmox-lxc"
+
+  node_name        = "proxmox"
+  container_id     = 201
+  hostname         = "vault"
+  template_file_id = proxmox_download_file.ubuntu_lxc.id
+
+  ip_address = "192.168.1.3/24"
+  gateway    = "192.168.1.1"
+
+  memory_mb    = 512
+  cpu_cores    = 1
+  disk_size_gb = 8
+
+  ssh_public_keys = [var.ssh_public_key]
+}
+
 variable "ssh_public_key" {
   type      = string
   sensitive = false
