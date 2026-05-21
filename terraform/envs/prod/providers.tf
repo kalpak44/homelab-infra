0be9150fbc390c6218dac54213d69d1f452bc3a6
Pavel Usanli <pavel.usanli@gmail.com>
2026-05-21 16:10:01 +0300

    add ssh agent to provider for disk import

--- terraform/envs/prod/providers.tf
+++ terraform/envs/prod/providers.tf
@@ -2,6 +2,11 @@ provider "proxmox" {
   endpoint  = var.proxmox_endpoint
   api_token = "${var.proxmox_username}=${var.proxmox_password}"
   insecure  = true
+
+  ssh {
+    username = "root"
+    agent    = true
+  }
 }
 
 variable "proxmox_endpoint" {
