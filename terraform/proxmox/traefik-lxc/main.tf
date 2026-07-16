af7388607aa9d74e4a16175ad4fd260e1915bb1a
Pavel Usanli <pavel.usanli@gmail.com>
2026-07-16 18:30:22 +0300

    add traefik-lxc terraform service

---
+++ terraform/proxmox/traefik-lxc/main.tf
@@ -0,0 +1,19 @@
+module "lxc" {
+  source = "../../modules/proxmox-lxc"
+
+  node_name        = "proxmox"
+  container_id     = 210
+  hostname         = "traefik"
+  template_file_id = var.lxc_template_file_id
+
+  ip_address = "192.168.1.10/24"
+  gateway    = "192.168.1.1"
+
+  cpu_cores    = 2
+  memory_mb    = 1024
+  disk_size_gb = 8
+
+  ssh_public_keys = [var.ssh_public_key]
+
+  password = var.host_password
+}
